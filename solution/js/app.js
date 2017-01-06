8f7a3d37cae219da3d6bde995e7cfdc28a83936e
Asus Zenbook 2015 <d.mijatovic@dv4all.nl>
2017-01-06 10:20:01 +0100

    inital commit

---
+++ solution/js/app.js
@@ -0,0 +1,19 @@
+(function(){
+'use strict'
+//define app
+let dmApp = angular.module('dmApp');
+
+//define controller 
+dmApp.controller('LunchCheckCtrl',[
+    //dependecies
+    '$scope',
+    //main controller function 
+    lunchCheckCtrl
+]);
+function lunchCheckCtrl(scope){
+
+    
+
+}
+
+})();
